dbc26fd61e81f552c585db86fccb1cd7b91a749b
“Tatiana124” <chernobaitatiana@gmail.com>
2025-08-10 14:04:28 +0300

    Виконані зміни

---
+++ lesson4/homework_04.py
@@ -0,0 +1,114 @@
+adwentures_of_tom_sawer = """\
+Tom gave up the brush with reluctance in his .... face but alacrity
+in his heart. And while 
+the late steamer
+"Big Missouri" worked ....
+and sweated
+in the sun,
+the retired artist sat on a barrel in the .... shade close by, dangled his legs,
+munched his apple, and planned the slaughter of more innocents.
+There was no lack of material;
+boys happened along every little while;
+they came to jeer, but .... remained to whitewash. ....
+By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for
+a kite, in good repair;
+and when he played
+out, Johnny Miller bought
+in for a dead rat and a string to swing it with—and so on, and so on,
+hour after hour. And when the middle of the afternoon came, from being a
+poor poverty, stricken boy in the .... morning, Tom was literally
+rolling in wealth."""
+
+# УВАГА! Перезаписуйте вміст змінної adwentures_of_tom_sawer у завданнях 01-03
+
+# task 01 ==
+""" Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
+треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
+adwentures_of_tom_sawer1 = adwentures_of_tom_sawer.replace('\n', ' ')
+print(adwentures_of_tom_sawer1.replace('\n', ' ')) 
+# task 02 ==
+adwentures_of_tom_sawer2 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+print(adwentures_of_tom_sawer2)
+# task 03 ==
+""" Зробіть так, щоб у тексті було не більше одного пробілу між словами.
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+print(final_text)
+
+# task 04
+""" Виведіть, скількі разів у тексті зустрічається літера "h"
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+count_h = final_text.count('h')
+print("кількість h у тексті" , count_h)
+
+# task 05
+""" Виведіть, скільки слів у тексті починається з Великої літери?
+підказка - порахувати кожну велику літеру напр, .count("A") і їх сумму
+"""
+"""
+я знайшла шо порахувати можна так, інші варіант був перебрати усі букви алфавіта (іх 26 аж) і тоді додати, але то трохи тупо
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+words = final_text.split()
+count = 0
+for word in words:
+    search_word = word.strip('.,;:!?()"“”—-')
+    if search_word[0].isupper():
+        count += 1
+
+print("Кількість слів що починаються з великої літери", count)
+
+# task 06
+""" Виведіть позицію, на якій слово Tom зустрічається вдруге
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+first_time = final_text.find('Tom')
+second_time = final_text.find('Tom', first_time + 1)
+print("слово Tom зустрічається на " , second_time ,"позиціі")
+
+# task 07
+""" Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
+Збережіть результат у змінній adwentures_of_tom_sawer_sentences
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+adwentures_of_tom_sawer_sentences = final_text.split(".")
+print(adwentures_of_tom_sawer_sentences)
+
+
+# task 08
+""" Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
+Перетворіть рядок у нижній регістр.
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+adwentures_of_tom_sawer_sentences = final_text.split(".")
+sentence_4 = adwentures_of_tom_sawer_sentences[3].strip()
+sentence_4_lower = sentence_4.lower()
+print(sentence_4_lower)
+
+# task 09
+""" Перевірте чи починається якесь речення з "By the time".
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+if final_text.find("By the time") :
+    print("У тексті є рядок, що починається з By the time ")
+else :
+    print("У тексті нема рядка, що починається з By the time ")   
+#тут,мабуть,якось треба було вкрутити startswith/endswith флу не придумала як.
+#у презинтаціі знайшла функцію if/else
+
+# task 10
+""" Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
+"""
+adwentures_of_tom_sawer3 = adwentures_of_tom_sawer.replace('....', ' ').replace('\n', ' ')
+final_text = ' '.join(adwentures_of_tom_sawer3.split())
+adwentures_of_tom_sawer_sentences = final_text.split(".")
+sentence_last = adwentures_of_tom_sawer_sentences[-1].strip()
+print(sentence_last)
